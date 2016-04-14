27409d5cf45a96897b290e7e9c53d28dcb610be5
Daniel <danielsilbs@gmail.com>
2016-04-14 14:12:37 -0400

    adding queue functionality to main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-loadData();
 
-function loadData () {
-    d3.csv("data/matches.csv", function (error, csv) {
-        console.log(csv);
-    })
-}
+queue()
+    .defer(d3.csv, "data/matches.csv")
+    .defer(d3.csv, "data/intraseason_data.csv")
+    .defer(d3.csv,"data/season_aggregate_stats.csv")
+    .await(function(error, matches, intraseason,aggregate) {
+
+        console.log(matches)
+        console.log(intraseason)
+        console.log(aggregate)
+
+    });
+
+
+
+
+
