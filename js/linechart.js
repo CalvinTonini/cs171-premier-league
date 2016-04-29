a2e97906ad50da7dc86c8582c2895386a6ee22ba
Daniel <danielsilbs@gmail.com>
2016-04-28 22:47:06 -0400

    reorganized html

--- js/linechart.js
+++ js/linechart.js
@@ -13,12 +13,12 @@ lineChart.prototype.initVis = function () {
     vis.margin = {
         top: 40,
         right: 40,
-        bottom: 60,
-        left: 60
+        bottom: 40,
+        left: 40
     };
 
-    vis.width = 700 - vis.margin.left - vis.margin.right;
-    vis.height = 600 - vis.margin.top - vis.margin.bottom;
+    vis.width = 1200 - vis.margin.left - vis.margin.right;
+    vis.height = 500 - vis.margin.top - vis.margin.bottom;
 
     vis.svg = d3.select("#" + vis.parentElement).append("svg")
         .attr("width", vis.width + vis.margin.left + vis.margin.right)
@@ -33,7 +33,9 @@ lineChart.prototype.initVis = function () {
 
     vis.xAxis = d3.svg.axis()
         .scale(vis.x)
+        .ticks(25)
         .orient("bottom");
+
     vis.yAxis = d3.svg.axis()
         .scale(vis.y)
         .orient("left");
